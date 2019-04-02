d5bfd53de9aa88af5d4c0f7d00525e728b8a91f8
Eric Pendergast <ependergast@mac.com>
2019-04-01 21:57:26 -0400

    Clean up raii.cpp

--- src/rendering/raii.cpp
+++ src/rendering/raii.cpp
@@ -1,8 +1,3 @@
 #include "raii.h"
 
-//std::unordered_map<FuncType, std::deque<TupleType>> RAII<stacks;
-//
-//template <>
-//std::unordered_map<WithViewport::FuncType, std::deque<WithViewport::TupleType>> WithViewport::stacks;
-//
 decltype(make_deques(functions)) stacks = make_deques(functions);
