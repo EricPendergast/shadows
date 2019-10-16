6645cb8003dac52740ffd5038ecaea949a4c869b
Eric Pendergast <ependergast@mac.com>
2019-10-16 18:13:31 -0400

    Remove static keyword

--- src/algorithms/vector_math.h
+++ src/algorithms/vector_math.h
@@ -5,39 +5,39 @@
 #include <iostream>
 
 namespace vec {
-    static void sub(double* x1, double* y1, double x2, double y2) {
+    void sub(double* x1, double* y1, double x2, double y2) {
         *x1 -= x2;
         *y1 -= y2;
     }
-    static void mult(double* x, double* y, double m) {
+    void mult(double* x, double* y, double m) {
         *x *= m;
         *y *= m;
     }
     
-    static double mag_sq(double x, double y) {
+    double mag_sq(double x, double y) {
         return x*x + y*y;
     }
     
-    static double mag(double x, double y) {
+    double mag(double x, double y) {
         return sqrt(mag_sq(x, y));
     }
     
-    static double dot(double x1, double y1, double x2, double y2) {
+    double dot(double x1, double y1, double x2, double y2) {
         return x1 * x2 + y1 * y2;
     }
     
-    static void project(double* proj_x, double* proj_y, double onto_x, double onto_y) {
+    void project(double* proj_x, double* proj_y, double onto_x, double onto_y) {
         double scale = dot(*proj_x, *proj_y, onto_x, onto_y)/mag_sq(onto_x, onto_y);
         *proj_x = onto_x * scale;
         *proj_y = onto_y * scale;
     }
     
-    static double proj_mag(double proj_x, double proj_y, double onto_x, double onto_y) {
+    double proj_mag(double proj_x, double proj_y, double onto_x, double onto_y) {
         project(&proj_x, &proj_y, onto_x, onto_y);
         return mag(proj_x, proj_y);
     }
     
-    static void reject(double* rej_x, double* rej_y, double onto_x, double onto_y) {
+    void reject(double* rej_x, double* rej_y, double onto_x, double onto_y) {
         double proj_x = *rej_x;
         double proj_y = *rej_y;
         project(&proj_x, &proj_y, onto_x, onto_y);
@@ -45,16 +45,16 @@ namespace vec {
         *rej_y -= proj_y;
     }
     
-    static double rej_mag(double rej_x, double rej_y, double onto_x, double onto_y) {
+    double rej_mag(double rej_x, double rej_y, double onto_x, double onto_y) {
         project(&rej_x, &rej_y, onto_x, onto_y);
         return mag(rej_x, rej_y);
     }
     
-    static double angle(double x1, double y1, double x2, double y2) {
+    double angle(double x1, double y1, double x2, double y2) {
         return acos(dot(x1, y1, x2, y2)/(mag(x1, y1)*mag(x2, y2)));
     }
     
-    static void norm(double x1, double y1, double* x2, double* y2) {
+    void norm(double x1, double y1, double* x2, double* y2) {
         *x2 = x1;
         *y2 = y1;
     }
